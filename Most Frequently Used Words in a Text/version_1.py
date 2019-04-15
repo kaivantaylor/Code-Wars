6a5ba5b330d27a6be4a0bb71fa1e51a7428f95f0
Kaivan Taylor <38149120+speedykai@users.noreply.github.com>
2019-04-14 21:56:46 -0400

    finished frequency counter for words in a text

--- Most Frequently Used Words in a Text/version_1.py
+++ Most Frequently Used Words in a Text/version_1.py
@@ -1,4 +1,10 @@
-text = 'hello my name is hello my name is marty'
+# text = '''In a village of La Mancha, the name of which I have no desire to call to
+# mind, there lived not long since one of those gentlemen that keep a lance
+# in the lance-rack, an old buckler, a lean hack, and a greyhound for
+# coursing. An olla of rather more beef than mutton, a salad on most
+# nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+# on Sundays, made away with three-quarters of his income.'''
+text = "e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"
 
 txtsplit = text.split()
 #print(txtsplit)
@@ -10,21 +16,14 @@ for str in txtsplit:
         txtdic[str] = txtdic[str] + 1
     else:
         txtdic[str] = 1
-
+print(txtdic)
 list = []
 for i in txtdic:
     list.append((txtdic[i],i))
 
 list.sort()
-list.reverse()
-#print(list)
+#list.reverse()
+print(list)
 
-count = 0
-final = []
-
-while count < 3:
-    a = list[count]
-    final.append(a[1])
-    count += 1
-
-print(final)
+number = 0
+final = {}
