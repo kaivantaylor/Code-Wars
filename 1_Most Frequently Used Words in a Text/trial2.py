850f991d9408d28044469ce3f4ebf5619fbe0c16
Kaivan Taylor <38149120+speedykai@users.noreply.github.com>
2019-04-15 01:11:52 -0400

    added dictionary

--- 1_Most Frequently Used Words in a Text/trial2.py
+++ 1_Most Frequently Used Words in a Text/trial2.py
@@ -4,32 +4,35 @@ def top_3_words(text):
     txtsplt = text.split()
     #print(txtsplt)
 
-    dict = {}
+    list = []
 
     for x in txtsplt:
         x = x.lower()
         #print(lower_x)
         x = x.strip(string.punctuation)
         #print(x)
+        list.append(x)
+    #print(list)
 
+    dict = {}
+
+    for x in list:
         if x in dict:
             dict[x] = dict[x] + 1
         else:
             dict[x] = int(1)
-    #print(dict)
-
-    list = []
+    newlist= []
     for x in dict:
-        list.append((dict[x],x))
-    list.sort()
-    list.reverse()
+        newlist.append((dict[x],x))
 
+    newlist.sort()
+    newlist.reverse()
     final = []
     count = 0
-    for x in list:
+    for x in newlist:
         if count < 3:
             if x[1] != '':
                 final.append(x[1])
                 count += 1
-
     return final
+print(top_3_words("ef  ei si se :e fesfs ax d s s d d s fe fag a se sd c"))
