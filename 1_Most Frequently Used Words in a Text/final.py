de6cafcfb783b9cc42057b0980b7b6ccfef258dd
Kaivan Taylor <38149120+speedykai@users.noreply.github.com>
2019-04-15 21:00:15 -0400

    added cases for string in between special characters and digits

---
+++ 1_Most Frequently Used Words in a Text/final.py
@@ -0,0 +1,47 @@
+import string
+import re
+
+def top_3_words(text):
+    print(text)
+    txtsplt = re.findall(r"[a-zA-Z']+", text)
+    #print(txtsplt)
+
+    list_1 = []
+    list_2= []
+    list_final = []
+    count = 0
+    dict = {}
+    words = 'bcdfghjklmnpqrstvxzaeiou'
+
+    for x in txtsplt:
+        if  (any(c in words for c in x.lower()) == True):
+            #print(x)
+            x = x.lower()
+            #print(lower_x)
+            x = x.strip(string.punctuation)
+            #print(x)
+            list_1.append(x)
+    #print(list_1)
+
+    for x in list_1:
+        if x in dict:
+            dict[x] = dict[x] + 1
+        else:
+            dict[x] = int(1)
+    print(dict)
+
+    for x in dict:
+        list_2.append((dict[x],x))
+
+    list_2.sort()
+    list_2.reverse()
+    list_2 = list_2[0:3]
+
+    print(list_2)
+    b = "''"
+    new = []
+    for x in list_2:
+        a = x[1]
+        new.append(a)
+    print(new)
+    return new
