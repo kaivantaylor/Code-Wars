3cea85447b60e3a1800093a073f7f669a2338cfd
Kaivan Taylor <38149120+speedykai@users.noreply.github.com>
2019-04-28 22:45:46 -0400

    added description of problem

--- 9_Hashtags/final.py
+++ 9_Hashtags/final.py
@@ -1,3 +1,14 @@
+# Kaivan Taylor
+# The marketing team is spending way too much time typing in hashtags.
+# Let's help them with out own Hashtag Generator!
+#
+# Here's the deal:
+#
+# It must start with a hashtag (#).
+# All words must have their first letter capitalized.
+# If the final result is longer than 140 chars it must return false.
+# If the input or the result is an empty string it must return false.
+
 def generate_hashtag(s):
     if s != '' and (len(s) <= 140):
         str_split = s.split()
