edd52d18c34e7535d795baf5b8a4dd2904d9678c
Kaivan Taylor <38149120+speedykai@users.noreply.github.com>
2019-04-30 11:09:04 -0400

    creating folders

---
+++ 10_Can you get the loop/example.py
@@ -0,0 +1,35 @@
+# Kaivan Taylor
+# Credits to the better answer from
+# u: larois, blackRobert, Joshy260
+# on code kata
+#
+# The user uses a O(N) complexity answer. It is almost linear and consumes
+# very little memory. This is used through a "keep-history"
+# method and allows the "turtle" to serve as a marker once they meet in the
+# middle.
+#
+#
+# def loop_size(node):
+#     turtle, rabbit = node.next, node.next.next
+#
+#     # Find a point in the loop.  Any point will do!
+#     # Since the rabbit moves faster than the turtle
+#     # and the kata guarantees a loop, the rabbit will
+#     # eventually catch up with the turtle.
+#     while turtle != rabbit:
+#         turtle = turtle.next
+#         rabbit = rabbit.next.next
+#
+#     # The turtle and rabbit are now on the same node,
+#     # but we know that node is in a loop.  So now we
+#     # keep the turtle motionless and move the rabbit
+#     # until it finds the turtle again, counting the
+#     # nodes the rabbit visits in the mean time.
+#     count = 1
+#     rabbit = rabbit.next
+#     while turtle != rabbit:
+#         count += 1
+#         rabbit = rabbit.next
+#
+#     # voila
+#     return count
