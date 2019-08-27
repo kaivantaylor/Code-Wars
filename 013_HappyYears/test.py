49b4c69b5ed3ff781b192ba5e053ed105912e335
Kaivan Taylor <38149120+speedykai@users.noreply.github.com>
2019-08-27 18:05:33 -0400

    wrote messy code, but it works

--- 013_HappyYears/test.py
+++ 013_HappyYears/test.py
@@ -2,4 +2,29 @@
 def some_func(year):
     if 1000 <= year <= 9000:
         print("Hello")
+        s = set()
 
+        
+        str_year = str(year)
+        print(str_year)
+        print(len(str_year))
+
+
+        for char in str_year:
+            s.add(char)
+    
+        print(s) # output of '0' and '5'
+        print(len(s))
+
+        if len(s) == 4:
+            print("this is a valid input")
+            print(year)
+        else:
+            print("this is not a valid input")
+            some_func(year + 1)
+            
+        # 5000
+    else:
+        print("The year is not valid")
+
+some_func(6060)
